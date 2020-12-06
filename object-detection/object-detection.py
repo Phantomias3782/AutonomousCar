26ea37024bc23144d370af73fc4965d7040bd37d
Phantomias3782 <50912413+Phantomias3782@users.noreply.github.com>
2020-12-06 11:19:58 +0100

    current object-detection state

--- object-detection/object-detection.py
+++ object-detection/object-detection.py
@@ -1,11 +1,14 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mping
+import time
+import imutils
 
 def load_coco_names():
+    "load names of coco text file"
 
     # create epmty list
-
     class_list = []
 
     # open coco txt file
@@ -13,11 +16,12 @@ def load_coco_names():
 
         class_list = [line.strip() for line in f.readlines()]
     
-    # return
+    # return list of classes
     return class_list
 
 
 def load_yolo(tiny = True):
+    "load yolo network. option to load tiny or normal one."
 
     print("started loading yolov3...")
 
@@ -43,6 +47,7 @@ def load_yolo(tiny = True):
     return output_layers, net
 
 def information_cal(outs, height, width):
+    "calculate objects in images"
 
     # Showing informations on the screen
     class_ids = []
@@ -75,27 +80,126 @@ def information_cal(outs, height, width):
     
     return w, h, x, y, boxes, confidences, class_ids
 
+def check_reaction(label):
+    "if label in speified list send signal"
+
+    # set list
+    check_list = ["person", "car"]
+
+    if label in check_list:
+
+        # reaction
+        print("Attention!")
+
+        return True
+
+    else:
+
+        return False
+
+def calculate_distance(image, object_width):
+    "get image and calculate distance to object"
+
+    # set marker attributes
+    marker_width = 16
+    marker_distance = 50
+
+    # check if calibration is done
+    if "focalLength" in globals():
+
+        # calcuate distance
+        distance = (marker_width * focalLength) / object_width
+
+        # return distance
+        return distance
+
+    else:
+
+        calibrate("./test_images/marker.jpg", marker_width, marker_distance)
+
+        # calcuate distance
+        distance = (marker_width * focalLength) / object_width
+
+        # return distance
+        return distance
+
+def calibrate(image, marker_width, marker_distance):
+    "calibrate first image from camera"
+
+    # fixed parameters in cm
+
+    image = mping.imread(image)
+
+    # convert the image to grayscale, blur it, detect edges
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 35, 125)
+
+	# find contures and keep largest (marker)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key = cv2.contourArea)
+
+	# compute marker
+    marker = cv2.minAreaRect(c)
+
+    # check edged image
+    # cv2.imwrite("gray_test.jpg", edged)
+    
+    # make global (db for one variable is oversized)
+    global focalLength
+
+    # calculate focalLength
+    focalLength = (marker[1][0] * marker_distance) / marker_width
+
+    # get distance to marker and print to check 
+    distance = (marker_width * focalLength) / marker[1][0]
+    print("marker distance ckeck", distance)
+
 def information_draw(boxes, confidences, class_ids, class_list, img):
 
-    #We use NMS function in opencv to perform Non-maximum Suppression
-    #we give it score threshold and nms threshold as arguments.
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    # set Non-maximum Suppression and normal threshold
+    threshold = 0.5
+    nms_threshold = 0.4
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, threshold, nms_threshold)
+
+    # generate color palette
     colors = np.random.uniform(0, 255, size=(len(class_list), 3))
+
+    # set font
     font = cv2.FONT_HERSHEY_PLAIN
 
     for i in range(len(boxes)):
+
         if i in indexes:
+
             x, y, w, h = boxes[i]
             label = str(class_list[class_ids[i]])
             color = colors[class_ids[i]]
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 3)
-            cv2.putText(img, label, (x, y -5), font,
-            1, color, 2)
+            cv2.putText(img, label, (x, y -5), font, 1, color, 2)
+
+            # send information if specific object in image
+            reaction = check_reaction(label)
+
+            if reaction:
+
+                # calculate distance
+                distance = calculate_distance(img, w)
+
+                print("distance to ", label, "is ", distance)
+
+                # interface to car movement! stop under certain distance to object
+
+            else:
+
+                print("No action required")
     
     # return edited image
     return img
 
-def detect_image(image_path):
+def detect_image(image_path, tiny=True):
 
     # set figure
     fig = plt.figure(figsize=(20, 10))
@@ -103,7 +207,7 @@ def detect_image(image_path):
     ax2 = fig.add_subplot(1,2,1, xticks = [], yticks = [])
 
     # Load and show original image
-    img_original = cv2.imread(image_path)
+    img_original = mping.imread(image_path)
     ax2.imshow(img_original)
     ax2.set_title("Original")
 
@@ -114,7 +218,7 @@ def detect_image(image_path):
     blob = cv2.dnn.blobFromImage(img_original, 1 / 255.0, (416, 416), swapRB=True, crop=False)
 
     # detect objects
-    output_layers, net = load_yolo(tiny=False)
+    output_layers, net = load_yolo(tiny=tiny)
     class_list = load_coco_names()
 
     net.setInput(blob)
@@ -126,8 +230,6 @@ def detect_image(image_path):
     # draw boxes and classification
     img = information_draw(boxes, confidences, class_ids, class_list, img_original)
 
-    #cv2.imshow("Image",img)
-
     # show edited image
     ax = fig.add_subplot(1,2,2, xticks = [], yticks = [])
     ax.set_title("Detected")
@@ -135,10 +237,58 @@ def detect_image(image_path):
     
     plt.show()
 
-    print("end")
+def detect_webcam(tiny=True):
+    
+    # get camera feed
+    video_capture = cv2.VideoCapture(0)
 
+    output_layers, net = object_detection.load_yolo(tiny=tiny)
+    class_list = object_detection.load_coco_names()
+
+    while True:
+
+        # get frame
+        re, frame = video_capture.read()
+
+        height, width, channels = frame.shape
+
+        # preprocess frame
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (320, 320), swapRB=True, crop=False)
+        
+        #Detecting objects
+        net.setInput(blob)
+        outs = net.forward(output_layers)
+
+        # calculate boxes and classifications
+        w, h, x, y, boxes, confidences, class_ids = information_cal(outs, height, width)
+
+        # draw boxes and classification
+        frame = information_draw(boxes, confidences, class_ids, class_list, frame)
+
+        # show feed
+        cv2.imshow("Image",frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+    video_capture.release()
+
+###############################################
 import os
 os.chdir("/Users/andreasmac/Documents/Github/AutonomousCar/object-detection")
 
-# detect_image("./test-images/neuhauser-strasse-detail.jpg")
-# detect_image("../lane_detection/google_img/google1.jpg")
+# directory = "../lane_detection/google_img"
+directory = "./test_images/"
+
+# calibrate(directory+"marker.jpg", 16, 50)
+
+detect_image(directory+"neuhauser-strasse-detail.jpg", tiny = False)
+
+# for image_path in list(os.listdir(directory)):
+
+#     try:
+#         detect_image(directory+image_path, tiny = False)
+#     except:
+#         print("Failed with image: ", image_path)
+
+# detect_webcam()
