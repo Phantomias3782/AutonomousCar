f576a8bb776cc08253cbcde2ebe448d76dd7b76d
Jan Brebeck <Jan.Brebeck98@gmail.com>
2020-12-14 19:12:19 +0100

    second stream for objectdetection

--- rasperry_stream/main.py
+++ rasperry_stream/main.py
@@ -40,6 +40,29 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
+@app.route('/objects')
+def index():
+    return render_template('index2.html') #you can customze index.html here
+
+def gen2(camera):
+    while True:
+        frame = camera.get_frame()
+        try:            
+            frame = detect_webcam(frame)
+        except Exception as e:
+            print("Error in detection")
+            print(e)
+        ret,frame=cv2.imencode(".jpg",frame) 
+        frame = frame.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed2')
+def video_feed():
+    return Response(gen2(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(pi_camera),
