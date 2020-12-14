1d88b8aa03117a60163491421de6302da6ac8e17
Jan Brebeck <Jan.Brebeck98@gmail.com>
2020-12-14 20:39:27 +0100

    added smoothing

--- rasperry_stream/run_car.py
+++ rasperry_stream/run_car.py
@@ -7,19 +7,26 @@ from lane_detection import lanedetect_steer
 
 camera = VideoCamera(flip=False) # flip pi camera if upside down.
 car = controll_car.Car()
-
+counter=0
+steering_mid=0
 while True:
     steering=0
     frame = camera.get_frame()
     try:
 
 	    frame2, steering=lanedetect_steer.lane_finding_pipeline(frame)
-
+        if steering!=None:
+            steering_mid+=steering
+        else:
+            counter-=1
 	    print(steering)
 
     except Exception as e:
 
        	print(e)
         print("steeringerror")
-    car.steer(steering)
+    if counter%5==0:
+        car.steer(steering_mid/5)
+        steering_mid=0
+    counter+=1
     
