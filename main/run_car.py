8554c68320d500d6b1444dca5c0300eedea09f9f
Jan Brebeck <Jan.Brebeck98@gmail.com>
2020-12-17 20:26:56 +0100

    testing vgg for OD

--- main/run_car.py
+++ main/run_car.py
@@ -5,7 +5,7 @@ import logging
 import threading
 from lane_detection import lanedetect_steer
 from object_detection import detect_webcam,detect_webcam_delay
-import OD
+import OD2
 
 camera = VideoCamera(flip=False) # flip pi camera if upside down.
 car = controll_car.Car()
@@ -17,7 +17,7 @@ while True:
         if not object_thread.is_alive():
             print("objectdetection started")
             # object_thread = threading.Thread(target=detect_webcam_delay, args=(frame,))
-            object_thread = threading.Thread(target=OD.detect, args=(frame,))
+            object_thread = threading.Thread(target=OD2.detect, args=(frame,))
             object_thread.start()
         frame2, steering=lanedetect_steer.lane_finding_pipeline(frame)
         car.steer(steering)
