ae452361f1b68fd3ae492f102fdf1ece8abd815c
Jan Brebeck <Jan.Brebeck98@gmail.com>
2020-12-17 20:41:33 +0100

    fixing other graph issue

--- main/OD2.py
+++ main/OD2.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
 from keras import optimizers
+import keras
 import numpy
 import os
 import numpy as np
@@ -11,6 +12,9 @@ import tensorflow as tf
 json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
+session = keras.backend.get_session()
+init = tf.global_variables_initializer()
+session.run(init)
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
 loaded_model.load_weights("model.h5")
@@ -33,5 +37,5 @@ def detect(frame):
     frame = cv2.resize(frame,(150,150))
     frame = frame.reshape((1,) + frame.shape)
     with graph.as_default():
-        pred = loaded_model.predict(img)
+        pred = loaded_model.predict(frame)
     print(pred)
